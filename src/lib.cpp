ed03264d8ddfb8a194603af4a449ea410ac40e4d
AbhiSivaprasad <abhi@live.com>
2020-03-06 11:45:51 -0500

    finished lib assembly

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,18 +1,22 @@
 #include "os.hpp"
 
 void print(char *buf) {
-	// r23, r24 are caller saved registers
+	// r23, is a caller saved register
 	// order of instructions is critical so use volatile
 	__asm__ volatile (
-		"lui 23,%2"
+		"ori 23,0,0"
+		"ori 23,23,%2"
 		"sh 23,10(0)"
+		"ori 23,0,0"
 		"lui 23,%3"
-		"sw 23, 12(0)"
-		"auipc 24, 0"
-		"addi 23, 24, 16"
-		"sw 23, 16(0)"
-		"jalr 24, 0, 0"
+		"ori 23,23,%4"
+		"sw 23,12(0)"
+		"ori 23,0,0"
+		"auipc 23,0"
+		"addi 23,23,16"
+		"sw 23,16(0)"
+		"jalr 23,0,0"
 		: 
-		: "r" (SYSCALL_PRINTF, buf)
+		: "r" (SYSCALL_PRINTF, (buf >> 12), (buf << 20) >> 20)
 	);
 }
